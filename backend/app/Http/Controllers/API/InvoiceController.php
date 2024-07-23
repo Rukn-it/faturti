5e5d1a9ae7698305c24dcb0bbfa5d2fcfe0599e5
MohammedMazher1 <muhmmed711300411@gmail.com>
2024-07-23 20:57:28 +0300

    edit interface path

--- backend/app/Http/Controllers/API/InvoiceController.php
+++ backend/app/Http/Controllers/API/InvoiceController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Classes\ApiResponseClass;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\InvoiceResource;
-use App\Interfaces\interfaces\InvoiceRepositoryInterface;
+use App\Interfaces\InvoiceRepositoryInterface;
 use App\Repositories\InvoiceRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
--- backend/app/Interfaces/InvoiceRepositoryInterface.php
+++ backend/app/Interfaces/InvoiceRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Interfaces\interfaces;
+namespace App\Interfaces;
 
 interface InvoiceRepositoryInterface
 {
