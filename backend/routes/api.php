c93b2418e9bace718ab38d4d0070ef83138c4d21
MohammedMazher1 <muhmmed711300411@gmail.com>
2024-07-24 22:05:53 +0300

    do user authentication task register,login,logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,9 +4,16 @@ use App\Http\Controllers\API\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\UserAuthController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// auth
+Route::post('register',[UserAuthController::class,'register']);
+Route::post('login',[UserAuthController::class,'login']);
+Route::post('logout',[UserAuthController::class,'logout'])
+  ->middleware('auth:sanctum');
 
 
 Route::apiResource('/customers',CustomerController::class);
